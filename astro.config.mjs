402bcc97dae658d0796f1ffa4d552db1a0ce90c1
TheJethFX <thejethfx@gmail.com>
2023-12-10 21:28:34 -0500

    Remove unused import statement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
-import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
