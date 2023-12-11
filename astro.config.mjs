b35810f6fb682cbcf77cfc11cdb73fb866a909bb
TheJethFX <thejethfx@gmail.com>
2023-12-10 21:27:44 -0500

    Update Astro workflow and add CNAME file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
+import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
+  site: "https://jeth.dev",
 });
