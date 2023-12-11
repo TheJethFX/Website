50096747b4386adfccb480d8a85e06275899f6ed
TheJethFX <thejethfx@gmail.com>
2023-12-10 23:42:04 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
-  site: "https://jeth.dev",
+  site: "https://thejethfx.github.io/Website",
 });
